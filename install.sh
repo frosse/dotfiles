b7bf0dfbfd02ed1f97a99eb381f7e668782d7e13
Jarno Lindholm <jarno.lindholm@profitsoftware.com>
2020-05-16 13:11:10 +0300

    (WIP) Added install script for nvim part

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+#rm  ~/.config/nvim/init.vim
+ln -svf $BASEDIR/nvim/init.vim ~/.config/nvim/init.vim
+
+PLUGIN_DIR=~/.config/nvim/plugin/
+AFTER_DIR=~/.config/nvim/after/plugin/
+
+if [ ! -d $PLUGIN_DIR ]; then
+  mkdir $PLUGIN_DIR
+fi
+ln -svf $BASEDIR/nvim/plugin/* $PLUGIN_DIR
+
+if [ ! -d $AFTER_DIR ]; then
+  mkdir $AFTER_DIR
+fi
+ln -svf $BASEDIR/nvim/after/plugin/* $AFTER_DIR
