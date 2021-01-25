5d7ee4743bb91db20138b1ab5ce5acfd689aece8
Jarno Lindholm <lindholm.jarno@gmail.com>
2021-01-25 08:02:23 +0200

    Create init.el

---
+++ .emacs.d/init.el
@@ -0,0 +1,31 @@
+(require 'package)
+(require 'use-package)
+
+(setq use-package-always-ensure t)
+(package-initialize)
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://stable.melpa.org/packages/"))
+
+(use-package org
+  :ensure t)
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  :config
+  (evil-mode 1))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(evil-collection counsel ivy-rich ivy doom-modeline gruvbox-theme key-chord magit which-key yasnippet-snippets yasnippet-classic-snippets yasnippet helm evil use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(org-babel-load-file "/home/frosse/.emacs.d/myconf.org")
