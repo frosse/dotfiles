877ecb2f7004c1f0650649e6889a5ece781b7252
Jarno Lindholm <jarno.lindholm@profitsoftware.com>
2021-01-25 15:52:36 +0200

    more emacs stuff

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,10 +1,18 @@
 (require 'package)
-(require 'use-package)
 
-(setq use-package-always-ensure t)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
 (package-initialize)
-(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://stable.melpa.org/packages/"))
+(unless package-archive-contents
+ (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+   (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
 
 (use-package org
   :ensure t)
@@ -15,17 +23,19 @@
   (setq evil-want-keybinding nil)
   :config
   (evil-mode 1))
+
+(org-babel-load-file "/home/jarno/.emacs.d/myconfig.org")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(evil-collection counsel ivy-rich ivy doom-modeline gruvbox-theme key-chord magit which-key yasnippet-snippets yasnippet-classic-snippets yasnippet helm evil use-package)))
+   (quote
+    (docker-tramp evil-collection key-chord magit which-key counsel ivy-rich ivy doom-modeline all-the-icons use-package gruvbox-theme evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(org-babel-load-file "/home/frosse/.emacs.d/myconf.org")
