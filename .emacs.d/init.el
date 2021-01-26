0ca61bed22df73455386743574b93f3817e10430
Jarno Lindholm <lindholm.jarno@gmail.com>
2021-01-26 08:05:16 +0200

    and more emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -17,22 +17,14 @@
 (use-package org
   :ensure t)
 
-(use-package evil
-  :init
-  (setq evil-want-integration t)
-  (setq evil-want-keybinding nil)
-  :config
-  (evil-mode 1))
-
-(org-babel-load-file "/home/jarno/.emacs.d/myconfig.org")
+(org-babel-load-file "/home/frosse/.emacs.d/myconfig.org")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (docker-tramp evil-collection key-chord magit which-key counsel ivy-rich ivy doom-modeline all-the-icons use-package gruvbox-theme evil))))
+   '(evil-magit projectile vterm docker-tramp evil-collection key-chord magit which-key counsel ivy-rich ivy doom-modeline all-the-icons use-package gruvbox-theme evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
