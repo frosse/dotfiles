7ccf7b2f3afd12a468c299eedc535c2772fe24bb
frosse <lindholm.jarno@gmail.com>
2021-03-10 19:06:03 +0200

    removed gaps and set border to 0

---
+++ .i3/scripts/i3exit.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+lock() {
+    i3lock
+}
+
+case "$1" in
+    lock)
+        lock
+        ;;
+    logout)
+        i3-msg exit
+        ;;
+    suspend)
+        systemctl suspend
+        ;;
+    hibernate)
+        systemctl hibernate
+        ;;
+    reboot)
+        systemctl reboot
+        ;;
+    shutdown)
+        systemctl poweroff
+        ;;
+    *)
+        echo "Usage: $0 [lock|logout|suspend|hibernate|reboot|shutdown]"
+        exit 2
+esac
+
+exit 0
